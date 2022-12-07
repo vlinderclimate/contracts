7e9efb69478b905f66eb7e137651e31307d59346
Roman Chuprikov <paradise.develop@gmail.com>
2022-12-07 21:31:31 +0500

    Add mint task

--- tasks/deploy-forward-token.ts
+++ tasks/deploy-forward-token.ts
@@ -5,6 +5,7 @@ import { task, types } from 'hardhat/config';
 task('deploy-forward', 'Deploys vlider forward token')
   .addParam('name', 'Token name')
   .addParam('symbol', 'Token symbol')
+  .addParam('description', 'Token description')
   .addParam('settlement', 'Settlement date UNIX timestamp', 0, types.int)
   .setAction(async (taskArgs, { ethers, upgrades }) => {
     const VlinderForwardToken = await ethers.getContractFactory(
@@ -13,12 +14,35 @@ task('deploy-forward', 'Deploys vlider forward token')
 
     const vlinderForwardToken = await upgrades.deployProxy(
       VlinderForwardToken,
-      [taskArgs.name, taskArgs.symbol, taskArgs.settlement]
+      [
+        taskArgs.name,
+        taskArgs.symbol,
+        taskArgs.description,
+        taskArgs.settlement,
+      ]
     );
 
     await vlinderForwardToken.deployed();
 
     console.log(
       `Forward token ${taskArgs.name} - ${taskArgs.symbol} is deployed! ${vlinderForwardToken.address}`
+    );
+  });
+
+task('mint-forward', 'Mint vlider forward token')
+  .addParam('address', 'Token address')
+  .addParam('to', 'Mint to address')
+  .addParam('amount', 'Amount in tokens')
+  .setAction(async (taskArgs, { ethers }) => {
+    const contract = await ethers.getContractAt(
+      'VlinderForwardToken',
+      taskArgs.address
+    );
+
+    const decimals = await contract.decimals();
+
+    await contract.mint(
+      taskArgs.to,
+      ethers.utils.parseUnits(taskArgs.amount, decimals)
     );
   });
